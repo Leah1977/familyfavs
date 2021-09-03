0e932fc4f0ac2db7bf104c90bd50ebcfddb6b660
Leah1977 <lh020277@gmail.com>
2021-09-03 18:18:51 +0000

    add more sections to submit in app route for edit recipe

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def create_recipe():
         }
         mongo.db.recipes.insert_one(recipe)
         flash("Recipe successfully added")
-        return redirect(url_for("get_recipes"))
+        return redirect(url_for("profile", username=session['user']))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("create_recipe.html", categories=categories)
@@ -157,12 +157,18 @@ def edit_recipe(recipe_id):
             "recipe_name": request.form.get("recipe_name"),
             "category_name": request.form.get("category_name"),
             "method": request.form.get("method"),
-            "ingredients": request.form.getlist("ingredients"),
-            "created_by": session["user"]
+            "ingredients": request.form.get("ingredients"),
+            "created_by": session["user"],
+            "recipe_img_url": request.form.get("recipe_img_url"),
+            "recipe_url": request.form.get("recipe_img"),
+            "serves": request.form.get("serves"),
+            "prep_time": request.form.get("prep_time"),
+            "serves": request.form.get("serves")
         }
         # update edited recipe to the database
         mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, submit)
         flash("Recipe successfully updated")
+        return redirect(url_for("profile", username=session['user']))
 
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
