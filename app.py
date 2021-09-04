5fe29528fe64531ec9a41c0df5d454228455eb70
Leah1977 <lh020277@gmail.com>
2021-09-04 20:08:39 +0000

    add modal description to profile page

--- app.py
+++ app.py
@@ -271,4 +271,4 @@ def myerror(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False) 
+            debug=False)
